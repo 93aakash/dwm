37204d49ec2503262a0535633e018a61ace9463a
Aakash Naugaraiya <93aakash@gmail.com>
2020-06-22 20:35:45 +0530

    Set font, tags and stuff

--- config.h
+++ config.h
@@ -2,21 +2,32 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=9" };
+static const char *fonts[]          = { "monospace:style=bold:size=8" };
 static const char dmenufont[]       = "monospace:size=9";
-static const char col_gray1[]       = "#111e2b";
-static const char col_gray2[]       = "#182738";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#979ead";
 static const char col_gray4[]       = "#acb2bf";
-static const char col_cyan[]        = "#5e82b5";
+static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_cyan, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
+/* static const char col_gray1[]       = "#111e2b"; */
+/* static const char col_gray2[]       = "#182738"; */
+/* static const char col_gray3[]       = "#979ead"; */
+/* static const char col_gray4[]       = "#acb2bf"; */
+/* static const char col_cyan[]        = "#5e82b5"; */
+/* static const char *colors[][3]      = { */
+/* 	/1*               fg         bg         border   *1/ */
+/* 	[SchemeNorm] = { col_cyan, col_gray1, col_gray2 }, */
+/* 	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan  }, */
+/* }; */
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -26,9 +37,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance          title       tags mask       isfloating   monitor */
-    { "Gimp",     NULL,             NULL,       0,              1,           -1 },
-	{ "firefox",  "Navigator",      NULL,       1 << 1,         0,           -1 },
+	/* class        instance              title       tags mask       isfloating   monitor */
+    { "Gimp",       NULL,                 NULL,       0,              1,           -1 },
+	{ "firefox",    "Navigator",          NULL,       1 << 1,         0,           -1 },
+	{ "Zathura",    "org.pwmt.zathura",   NULL,       1 << 2,         0,           -1 },
+	{ "mpv",        "gl",                 NULL,       1 << 3,         0,           -1 },
 };
 
 /* layout(s) */
@@ -68,7 +81,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_u,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_p,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_space,  zoom,           {0} },
@@ -108,7 +121,9 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button1,        sigdwmblocks,   {.i = 1} },
+	{ ClkStatusText,        0,              Button2,        sigdwmblocks,   {.i = 2} },
+	{ ClkStatusText,        0,              Button3,        sigdwmblocks,   {.i = 3} },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
@@ -116,5 +131,7 @@ static Button buttons[] = {
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+    { ClkTagBar,		    0,		        Button4,	    shiftview,	    {.i = -1} },
+	{ ClkTagBar,		    0,		        Button5,	    shiftview,	    {.i = 1} },
 };
 
