588e8cc91341306174aa0cb799d24d94945a88e2
Aakash Naugaraiya <93aakash@gmail.com>
2020-07-28 00:16:35 +0530

    Tweaked the colors

--- config.def.h
+++ config.def.h
@@ -3,19 +3,26 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 7;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 7;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:style=bold:size=8" };
+static const char *fonts[]          = {"monospace:style=Bold:size=8"};
 static const char dmenufont[]       = "monospace:size=9";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray1[]       = "#191d21";
+static const char col_gray2[]       = "#343e47";
 static const char col_gray3[]       = "#979ead";
 static const char col_gray4[]       = "#acb2bf";
-static const char col_cyan[]        = "#005577";
+/* static const char col_cyan[]        = "#005577"; */
+static const char col_cyan[]        = "#004777";
+static const char col_blue[]        = "#566faf";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_blue  },
 };
 
 /* tagging */
@@ -56,8 +63,10 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
+static char dmenulines[] = "13";
+static char dmenuprompt[] = "run:";
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-i", "-l", dmenulines, "-p", dmenuprompt, "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "urxvt", "-title", scratchpadname, "-geometry", "100x28", NULL };
