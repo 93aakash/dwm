497823f203faf4f25c4d7a4aa6607a7642e9b295
Aakash Naugaraiya <93aakash@gmail.com>
2020-08-03 19:17:12 +0530

    Made some tweaks. Again.

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"monospace:style=Bold:size=8"};
-static const char dmenufont[]       = "monospace:size=9";
+static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#191d21";
 static const char col_gray2[]       = "#343e47";
 static const char col_gray3[]       = "#979ead";
@@ -26,7 +26,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 
 static const Rule rules[] = {
@@ -66,7 +66,10 @@ static const Layout layouts[] = {
 static char dmenulines[] = "13";
 static char dmenuprompt[] = "run:";
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-i", "-l", dmenulines, "-p", dmenuprompt, "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-i", "-l", dmenulines, "-p", 
+                                dmenuprompt, "-m", dmenumon, "-fn", dmenufont,
+                                "-nb", col_gray1, "-nf", col_gray3, "-sb", 
+                                col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "urxvt", "-title", scratchpadname, "-geometry", "100x28", NULL };
