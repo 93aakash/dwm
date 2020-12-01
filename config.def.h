c12e3b2f605e990248cc08914446f9358951dba5
Aakash Naugaraiya <93aakash@gmail.com>
2020-12-01 16:32:12 +0530

    Removed redundant lines

--- config.def.h
+++ config.def.h
@@ -3,12 +3,13 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
+
 /* static const unsigned int gappih    = 7;       /1* horiz inner gap between windows *1/ */
 /* static const unsigned int gappiv    = 7;       /1* vert inner gap between windows *1/ */
 /* static const unsigned int gappoh    = 10;       /1* horiz outer gap between windows and screen edge *1/ */
 /* static const unsigned int gappov    = 10;       /1* vert outer gap between windows and screen edge *1/ */
 /* static const int smartgaps          = 0;        /1* 1 means no outer gap when there is only one window *1/ */
+
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"Ubuntu Mono:style=Bold:size=9"};
@@ -35,10 +36,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class        instance            title   tags mask   isfloating  monitor */
-	/* class     instance       title       tags mask   isfloating      isterminal      noswallow   monitor */
-	{ "firefox",    "Navigator",        NULL,   1 << 1,     0,          0,              1,          -1 },
-	{ "mpv",        "gl",               NULL,   0,          1,          0,              0,          -1 },
-	/* { "St",      NULL,     NULL,           0,         0,          1,           0,        -1 }, */
+	{ "firefox",    "Navigator",        NULL,   1 << 1,     0,          -1 },
+	{ "mpv",        "gl",               NULL,   0,          1,          -1 },
 };
 
 /* layout(s) */
